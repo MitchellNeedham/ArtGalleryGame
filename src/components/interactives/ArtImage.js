58ca6bb470ac2ff771ead6daba2dc5afef628ef3
Mitchell Needham <mneedham03@gmail.com>
2021-11-08 16:26:08 +1100

    Added variable lighting in theatre

--- src/components/interactives/ArtImage.js
+++ src/components/interactives/ArtImage.js
@@ -23,7 +23,7 @@ export default function ArtImage(props) {
         }
       }
       onClick={
-        () => addInteractionBox(() => <ArtImageView {...props} />, '70vw')
+        () => addInteractionBox(() => <ArtImageView {...props} />, '700px')
       }
     />
   )
--- src/components/interactives/ArtVideo.js
+++ src/components/interactives/ArtVideo.js
@@ -1,24 +1,70 @@
+import { useEffect, useState, useRef } from "react";
+
 export default function ArtVideo(props) {
   const {
     pos,
     dimensions,
     path
   } = props;
+  const [darkRoom, setDarkRoom] = useState(false);
+  const darkenRef = useRef(null);
+
+  const onPlayerStateChange = (event) => {
+    if (event.data === window.YT.PlayerState.PLAYING) {
+      setDarkRoom(true);
+      return;
+    };
+    setDarkRoom(false);
+  }
+
+  useEffect(() => {
+    window.YT.ready(() => {
+      new window.YT.Player('player', {
+        width: window.innerHeight * dimensions[0],
+        height: window.innerHeight * dimensions[1],
+        videoId: path,
+        playerVars: {
+          rel: 0
+        },
+        events: {
+          onStateChange: onPlayerStateChange
+        }
+      });
+    });    
+  }, []);
+
+  
+
   return (
-      <iframe
-        width={window.innerHeight * dimensions[0]}
-        height={window.innerHeight * dimensions[1]}
-        src={path}
-        frameBorder="0"
+    <div
+      ref={darkenRef}
+      className={'theatre-overlay' + (darkRoom ? ' lights-out' : ' lights-on')}
+      style={
+        {
+          position:'relative',
+          width: '100%',
+          height: '100%',
+          //backgroundColor: darkRoom ? '#000a' : '#0000',
+          mixBlendMode: 'multiply',
+        }
+      }
+    >
+      <div
         style={
           {
             position: 'absolute',
             left: pos[0] * 100 + 'vh',
-            top: pos[1] * 100 + 'vh'
+            top: pos[1] * 100 + 'vh',
+            width: window.innerHeight * dimensions[0] + 'px',
+            height: window.innerHeight * dimensions[1] + 'px',
+            boxShadow: `0 100px 200px 100px ${darkRoom ? '#fff3' : '#0000'}`,
           }
         }
-        title="Video"
-        allowFullScreen
-      ></iframe>
+      >
+        <div
+          id="player"
+        ></div>
+      </div>
+    </div>
   )
 }
