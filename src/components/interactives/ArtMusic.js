1b8ff3b1875e932b8704d99c8b9c315fa2391f6e
Mitchell Needham <mneedham03@gmail.com>
2021-11-15 20:21:38 +1100

    Updated music player

--- src/components/interactives/ArtMusic.js
+++ src/components/interactives/ArtMusic.js
@@ -5,11 +5,16 @@ import { useSceneLoaded } from "../../api/LoadedContext";
 import { useInteractionBoxUpdate } from "../../api/InteractionBoxContext";
 
 import MusicSelection from "./MusicSelection";
+import AudioPlayer from "react-h5-audio-player";
+import 'react-h5-audio-player/src/styles.scss';
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
 
 export default function ArtMusic(props) {
   const {
-    selectpos,
-    playpos
+    selectpos
   } = props;
   const audioRef = useRef(null);
   const [playing, setPlaying] = useState(false);
@@ -25,10 +30,10 @@ export default function ArtMusic(props) {
 
   useEffect(() => {
     if (playing) {
-      audioRef.current?.play();
+      audioRef.current?.audio.current.play();
       return;
     }
-    audioRef.current?.pause();
+    audioRef.current?.audio.current.pause();
   }, [playing]);
 
   useEffect(() => {
@@ -39,6 +44,11 @@ export default function ArtMusic(props) {
     addInteractionBox(
       (closeUI) => (<MusicSelection closeUI={closeUI} />)
     , '1000px');
+  }
+
+  const handleChangeSong = (change) => {
+    const newSong = mod(activeMusic + change, music.length);
+    changeMusic(newSong);
   }
 
   return (
@@ -57,36 +67,25 @@ export default function ArtMusic(props) {
       >
         Songs
       </div>
-      <div
-        className="song-selection art"
-        style={
-          {
-            position: 'absolute',
-            top: playpos[1] * 100 + 'vh',
-            left: playpos[0] * 100 + 'vh',
-          }
-        }
-        onClick={() => setPlaying(!playing)}
-        onKeyUp={(e) => e.key === 'Enter' && setPlaying(!playing)}
-      >
-        {playing ? 'Pause' : 'Play'}
-      </div>
       {isLoaded &&
-        <audio
-          style={
-            {
-              position: 'absolute',
-              top: selectpos[1] * 100 + 'vh',
-              left: selectpos[0] * 100 + 'vh',
-            }
-          }
-          ref={audioRef}
-          src={music[activeMusic]?.path}
-          onCanPlayThrough={(e) => audioRef.current?.play()}
-          onEnded={() => changeMusic(activeMusic + 1)}
-        >
-        </audio>
-      }
+      (
+        <div className="music-player" >
+          <p>
+            {`${music[activeMusic]?.title} - ${music[activeMusic]?.artist}`}
+          </p>
+          <AudioPlayer
+            src={music[activeMusic]?.path}
+            ref={audioRef}
+            showJumpControls={false}
+            showSkipControls
+            onClickNext={() => handleChangeSong(1)}
+            onClickPrevious={() => handleChangeSong(-1)}
+            onCanPlayThrough={() => audioRef.current?.audio.current.play()}
+            onEnded={() => handleChangeSong(1)}
+            customAdditionalControls={[]}
+          />
+        </div>
+      )}
     </>
   )
 }
