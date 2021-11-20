b8975f1870e622cd90a0ed04b063bbfdb5209d25
Mitchell Needham <mneedham03@gmail.com>
2021-11-20 14:59:24 +1100

    Fixed npc and clouds

--- src/components/interactives/SpeechBubble.js
+++ src/components/interactives/SpeechBubble.js
@@ -1,35 +1,45 @@
-import { useEffect, useState } from "react";
-import { useCharacter } from "../../api/CharacterContext";
+import { useState } from "react";
 
 export default function SpeechBubble(props) {
   const {
     image,
-    posX,
-    range,
+    pos,
+    dim,
   } = props;
-  const { charPos } = useCharacter();
   const [visible, setVisible] = useState(false);
 
-
-  useEffect(() => {
-    setVisible(charPos[0]/100 > posX - range && charPos[0]/100 < posX + range);
-  }, [charPos, posX, range]);
-
   return (
-    <img
-      style={
-        {
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          display: visible ? 'block' : 'none',
-          pointerEvents: 'none'
+    <>
+      <img
+        style={
+          {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: visible ? 'block' : 'none',
+            pointerEvents: 'none'
+          }
         }
-      }
-      src={image}
-      alt="speech bubble"
-    />
+        src={image}
+        alt="speech bubble"
+      />
+      <div
+        style={
+          {
+            position: 'absolute',
+            top: pos[1] * 100 + 'vh',
+            left: pos[0] * 100 + 'vh',
+            height: dim[1] * 100 + 'vh',
+            width: dim[0] * 100 + 'vh'
+          }
+        }
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+      >
+
+      </div>
+    </>
   )
 }
